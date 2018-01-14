8c8aa4e0bf713664f8d5a046f822d88f3abc78de
lynzt <techells22+github@gmail.com>
2018-01-13 20:22:01 -0600

    add script to rename files

--- rename_pics.py
+++ rename_pics.py
@@ -1,17 +1,30 @@
+import os
+import re
 import sys
-from file_utils import *
+from file_utils import file_utils
 
-scanned_dir = '~/Pictures/scanned'
+def get_dir_files(path):
+    ctime = lambda f: os.stat(os.path.join(path, f)).st_ctime
+    files = list(sorted(file_utils.get_files_in_directory_skip_hidden(path), key=ctime)) # files listed in created order
 
-def get_dir_files(url):
-
-    r = requests.get(url)
-    return r.text
+    regex = re.compile(r'Scan(\s\d+)?')
+    return list(filter(regex.match, files)) # don't rename files already renamed
 
 
+def rename_files(files, starting_nbr, tmp_dir):
+    for filename in files:
+        print ('renaming {}'.format(filename))
+        file_utils.move_file(tmp_dir + filename, tmp_dir + 'scan_' + str(starting_nbr) + '.jpeg')
+        starting_nbr += 1
 
 def main():
-    files = get_dir_files()
+    starting_nbr = 100761 # this could be figured out... meh
+    tmp_dir = '/tmp/'
+
+    files = get_dir_files(tmp_dir)
+    rename_files(files, starting_nbr, tmp_dir)
 
 if __name__ == '__main__':
-    sys.exit(main()
+    main()
+
+# docker run -it --rm -v "$PWD":/usr/src/app -v "/Users/lynzt/Pictures/scanned":/tmp py/rename python3 ./rename_pics.py
